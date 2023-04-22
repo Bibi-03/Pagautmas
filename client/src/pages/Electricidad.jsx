17e3e97d933ca47710ca6a3af1a784815dc9278b
Bibi-03 <brenquesada22@gmail.com>
2023-04-22 09:54:53 -0600

    build

--- client/src/pages/Electricidad.jsx
+++ client/src/pages/Electricidad.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { SMTPClient} from 'emailjs';
 
 function Electricidad() {
   return (
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-// import { dirname, join } from 'path'
-// import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 import { PORT } from './config.js'
 
 import LoginRoutes from './routes/Login.routes.js'
@@ -10,8 +10,8 @@ import RealizarPagoRoutes from './routes/RealizarPago.routes.js'
 import CuentaBancariaRoutes from './routes/CuentaBancaria.routes.js'
 
 const app = express()
-// const __dirname = dirname(fileURLToPath(import.meta.url))
-// console.log(__dirname)
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname)
 
 app.use(cors())
 app.use(express.json())
@@ -21,11 +21,11 @@ app.use('/api', ValidarCuentaRoutes)
 app.use('/api', RealizarPagoRoutes)
 app.use('/api', CuentaBancariaRoutes)
 
-// app.use(express.static(join(__dirname, '../client/dist')))
+app.use(express.static(join(__dirname, '../client/dist')))
 
-// app.get('*', (req, res) => {
-//   res.sendFile(join(__dirname, '../client/dist/index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '../client/dist/index.html'))
+})
 
 app.listen(PORT)
 console.log(`Server is listening on port ${PORT}`)
